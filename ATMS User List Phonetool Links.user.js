03fc5edd174f5042d8ece065632eefe8815f8abd
abuffet-amz <abuffet@amazon.com>
2025-02-19 12:01:57 +0000

    Update ATMS User List Phonetool Links.user.js

--- ATMS User List Phonetool Links.user.js
+++ ATMS User List Phonetool Links.user.js
@@ -7,8 +7,8 @@
 // @match        https://atms.a2z.com/web/user/list*
 // @match        https://web.atms.a2z.com/web/user/list*
 // @grant        none
-// @updateURL    https://github.com/abuffet-amz/Script/raw/refs/heads/main/ATMS%20User%20List%20Phonetool%20Links.js
-// @downloadURL  https://github.com/abuffet-amz/Script/raw/refs/heads/main/ATMS%20User%20List%20Phonetool%20Links.js
+// @updateURL    https://github.com/abuffet-amz/Script/raw/refs/heads/main/ATMS%20User%20List%20Phonetool%20Links.user.js
+// @downloadURL  https://github.com/abuffet-amz/Script/raw/refs/heads/main/ATMS%20User%20List%20Phonetool%20Links.user.js
 // @run-at       document-end
 // ==/UserScript==
 
